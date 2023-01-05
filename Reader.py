f1bb5832d231c29fa3c5f993768fd54635eb3648
NicChevrier <94065816+NicChevrier@users.noreply.github.com>
2023-01-05 09:43:06 -0400

    Create Reader.py

---
+++ Reader.py
@@ -0,0 +1,12 @@
+import random
+
+# List of tarot cards
+tarot = ["The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor",
+         "The Hierophant", "The Lovers", "The Chariot", "Strength", "The Hermit",
+         "The Wheel of Fortune", "Justice", "The Hanged Man", "Death", "Temperance",
+         "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgment", "The World"]
+
+# Choose a random tarot card
+card = random.choice(tarot)
+
+print(card)
